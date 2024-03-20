fb0e7df2e0f00983995f53df898c9c9b6f89bd22
Rose, Michael <Michael.Rose@e-hps.com>
2024-03-20 14:28:37 -0700

    Fixed the alert messages.

--- src/Abp.Web.Common/Web/Mvc/Alerts/PageAlertMessageRenderer.cs
+++ src/Abp.Web.Common/Web/Mvc/Alerts/PageAlertMessageRenderer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using Abp.Dependency;
 
 namespace Abp.Web.Mvc.Alerts
 {
@@ -12,16 +9,30 @@ namespace Abp.Web.Mvc.Alerts
 
         public virtual string Render(List<AlertMessage> alertList)
         {
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             foreach (var alertMessage in alertList)
             {
-                sb.Append($"<div class=\"alert alert-{alertMessage.Type.ToString().ToLowerInvariant()}\" {(alertMessage.Dismissible ? "alert-dismisable" : "")} role=\"alert\">" +
-                              $"<h4 class=\"alert-heading\">{alertMessage.Title}" +
-                                  $"{(alertMessage.Dismissible ? "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" : "")}" +
-                              $"</h4>" +
-                                $"<p>{alertMessage.Text}</p>" +
-                          $"</div>");
+                var mType = alertMessage.Type.ToString().ToLowerInvariant();
+
+                if (alertMessage.Dismissible)
+                {
+                    sb.Append($"<div class=\"alert alert-{mType} alert-dismisable\" role=\"alert\">");
+                    sb.Append($"<h4 class=\"alert-heading\">{alertMessage.Title}");
+                    sb.Append($"<button type=\"button\" class=\"close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>");
+                    sb.Append($"</h4>");
+                    sb.Append($"<p>{alertMessage.Text}</p>");
+                    sb.Append($"</div>");
+                }
+                else
+                {
+                    sb.Append($"<div class=\"alert alert-{mType}\" role=\"alert\">");
+                    sb.Append($"<h4 class=\"alert-heading\">{alertMessage.Title}");
+                    sb.Append($"</h4>");
+                    sb.Append($"<p>{alertMessage.Text}</p>");
+                    sb.Append($"</div>");
+                }
             }
+
             return sb.ToString();
         }
     }
--- src/Abp.Web.Common/Web/Mvc/Alerts/ToastrAlertMessageRenderer.cs
+++ src/Abp.Web.Common/Web/Mvc/Alerts/ToastrAlertMessageRenderer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace Abp.Web.Mvc.Alerts
@@ -11,18 +9,22 @@ namespace Abp.Web.Mvc.Alerts
 
         public virtual string Render(List<AlertMessage> alertList)
         {
-            StringBuilder sb = new StringBuilder("<script type=\"text/javascript\">");
+            var sb = new StringBuilder("<script type=\"text/javascript\">");
+
             foreach (var alertMessage in alertList)
             {
-                sb.Append(
-                    string.Format("abp.notify.{0}('{1}','{2}', {{ 'closeButton':  {3} }});",
-                        alertMessage.Type.ToString().ToLowerInvariant()
-                            .Replace("warning", "warn")
-                            .Replace("danger", "error"),
-                        alertMessage.Text,
-                        alertMessage.Title,
-                        alertMessage.Dismissible.ToString().ToLower()));
+                var mType = alertMessage.Type.ToString().ToLowerInvariant().Replace("warning", "warn").Replace("danger", "error");
+
+                if (alertMessage.Dismissible)
+                {
+                    sb.AppendLine($"abp.notify.{mType}('{alertMessage.Text}','{alertMessage.Title}', {{ 'closeButton':  true, 'timeOut': 0, 'extendedTimeOut': 0 }});");
+                }
+                else
+                {
+                    sb.AppendLine($"abp.notify.{mType}('{alertMessage.Text}','{alertMessage.Title}', {{ 'closeButton':  false, 'timeOut': 5000, 'extendedTimeOut': 1000 }});");
+                }
             }
+
             sb.Append("</script>");
             return sb.ToString();
         }
